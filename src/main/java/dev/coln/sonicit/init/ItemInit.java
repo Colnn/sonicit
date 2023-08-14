5fc7872c293825ca18511720f4861900142ccbe3
_Coln <csoer1947@student.landstede.nl>
2023-08-15 01:55:16 +0200

    first commit

---
+++ src/main/java/dev/coln/sonicit/init/ItemInit.java
@@ -0,0 +1,39 @@
+package dev.coln.sonicit.init;
+
+import dev.coln.sonicit.SonicIt;
+import dev.coln.sonicit.items.SonicScrewdriverItem;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.RecordItem;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ItemInit {
+
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, SonicIt.MOD_ID);
+
+    public static final RegistryObject<Item> TEN_SCREWDRIVER = ITEMS.register("10_screwdriver",
+            () -> new SonicScrewdriverItem(new Item.Properties().stacksTo(1).tab(ModCreativeTab.instance)));
+
+    public static final RegistryObject<Item> ELEVEN_SCREWDRIVER = ITEMS.register("11_screwdriver",
+            () -> new SonicScrewdriverItem(new Item.Properties().stacksTo(1).tab(ModCreativeTab.instance)));
+    public static final RegistryObject<Item> DWHO_THEME = ITEMS.register("doctor_who_theme",
+            () -> new RecordItem(8, SoundInit.DWHO_THEME, new Item.Properties().stacksTo(1), 3880));
+
+    public static class ModCreativeTab extends CreativeModeTab {
+        private ModCreativeTab(int index, String label) {
+            super(index, label);
+        }
+
+        @Override
+        public ItemStack makeIcon() {
+            return new ItemStack(TEN_SCREWDRIVER.get());
+        }
+
+        public static final ModCreativeTab instance = new ModCreativeTab(CreativeModeTab.TABS.length, "sonicit");
+    }
+}
