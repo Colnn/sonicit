3adc3f71a32da5c0ced3fc08668b12c62cccd51c
Student Colin Soer <csoer1947@student.landstede.nl>
2023-08-22 20:27:59 +0200

    Added JEI integration

---
+++ src/main/java/dev/coln/sonicit/integration/JEISonicItPlugin.java
@@ -0,0 +1,39 @@
+package dev.coln.sonicit.integration;
+
+import dev.coln.sonicit.SonicIt;
+import dev.coln.sonicit.recipe.SonicWorkbenchRecipe;
+import mezz.jei.api.IModPlugin;
+import mezz.jei.api.JeiPlugin;
+import mezz.jei.api.recipe.RecipeType;
+import mezz.jei.api.registration.IRecipeCategoryRegistration;
+import mezz.jei.api.registration.IRecipeRegistration;
+import net.minecraft.client.Minecraft;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.crafting.RecipeManager;
+
+import java.util.List;
+import java.util.Objects;
+
+@JeiPlugin
+public class JEISonicItPlugin implements IModPlugin {
+    public static RecipeType<SonicWorkbenchRecipe> SONIC_TYPE =
+            new RecipeType<>(SonicWorkbenchRecipeCategory.UID, SonicWorkbenchRecipe.class);
+
+    @Override
+    public ResourceLocation getPluginUid() {
+        return new ResourceLocation(SonicIt.MOD_ID, "jei_plugin");
+    }
+
+    @Override
+    public void registerCategories(IRecipeCategoryRegistration registration) {
+        registration.addRecipeCategories(new
+                SonicWorkbenchRecipeCategory(registration.getJeiHelpers().getGuiHelper()));
+    }
+
+    @Override
+    public void registerRecipes(IRecipeRegistration registration) {
+        RecipeManager rm = Objects.requireNonNull(Minecraft.getInstance().level).getRecipeManager();
+        List<SonicWorkbenchRecipe> recipesSonic = rm.getAllRecipesFor(SonicWorkbenchRecipe.Type.INSTANCE);
+        registration.addRecipes(SONIC_TYPE, recipesSonic);
+    }
+}
---
+++ src/main/java/dev/coln/sonicit/integration/SonicWorkbenchRecipeCategory.java
@@ -0,0 +1,57 @@
+package dev.coln.sonicit.integration;
+
+import dev.coln.sonicit.SonicIt;
+import dev.coln.sonicit.init.BlockInit;
+import dev.coln.sonicit.init.ItemInit;
+import dev.coln.sonicit.recipe.SonicWorkbenchRecipe;
+import mezz.jei.api.constants.VanillaTypes;
+import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
+import mezz.jei.api.gui.drawable.IDrawable;
+import mezz.jei.api.helpers.IGuiHelper;
+import mezz.jei.api.recipe.IFocusGroup;
+import mezz.jei.api.recipe.RecipeIngredientRole;
+import mezz.jei.api.recipe.RecipeType;
+import mezz.jei.api.recipe.category.IRecipeCategory;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
+
+public class SonicWorkbenchRecipeCategory implements IRecipeCategory<SonicWorkbenchRecipe> {
+    public final static ResourceLocation UID = new ResourceLocation(SonicIt.MOD_ID, "sonic_workbench");
+    public final static ResourceLocation TEXTURE = new ResourceLocation(SonicIt.MOD_ID, "textures/gui/sonic_workbench_gui.png");
+
+    private final IDrawable background;
+    private final IDrawable icon;
+
+    public SonicWorkbenchRecipeCategory(IGuiHelper helper) {
+        this.background = helper.createDrawable(TEXTURE, 0, 0, 176, 85);
+        this.icon = helper.createDrawableIngredient(VanillaTypes.ITEM_STACK, new ItemStack(BlockInit.SONIC_WORKBENCH.get()));
+    }
+
+    @Override
+    public RecipeType<SonicWorkbenchRecipe> getRecipeType() {
+        return JEISonicItPlugin.SONIC_TYPE;
+    }
+
+    @Override
+    public Component getTitle() {
+        return Component.literal("Sonic Workbench");
+    }
+
+    @Override
+    public IDrawable getBackground() {
+        return this.background;
+    }
+
+    @Override
+    public IDrawable getIcon() {
+        return this.icon;
+    }
+
+    @Override
+    public void setRecipe(IRecipeLayoutBuilder builder, SonicWorkbenchRecipe recipe, IFocusGroup focuses) {
+        builder.addSlot(RecipeIngredientRole.INPUT, 86, 15).addIngredients(recipe.getIngredients().get(0));
+
+        builder.addSlot(RecipeIngredientRole.OUTPUT, 86, 60).addItemStack(recipe.getResultItem());
+    }
+}
