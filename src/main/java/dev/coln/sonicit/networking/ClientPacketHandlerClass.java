cb6cacf11a7b400a6d5ebbee2722c64bb3c61c29
_Coln <colinsoer@gmail.com>
2023-08-30 14:44:02 +0200

    Changed sonic sound to neutral

--- src/main/java/dev/coln/sonicit/networking/ClientPacketHandlerClass.java
+++ src/main/java/dev/coln/sonicit/networking/ClientPacketHandlerClass.java
@@ -14,6 +14,6 @@ public class ClientPacketHandlerClass {
     public static void handlePacket(SonicSoundS2CPacket msg, Supplier<NetworkEvent.Context> ctx) {
         RandomSource randomSource = RandomSource.create();
         float random = Mth.randomBetween(randomSource, 0.5f, 2.0f);
-        Minecraft.getInstance().player.getLevel().playSound(Minecraft.getInstance().player, msg.blockPos, SoundInit.SONIC_SOUND.get(), SoundSource.PLAYERS, 1, random);
+        Minecraft.getInstance().player.getLevel().playSound(Minecraft.getInstance().player, msg.blockPos, SoundInit.SONIC_SOUND.get(), SoundSource.NEUTRAL, 1, random);
     }
 }
